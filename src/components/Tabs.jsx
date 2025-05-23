131c119ddc8e5d61eba2efed5b860d21786cdca7
Vignesh1hp <vigneshkumarb.sc2004@gmail.com>
2025-05-23 12:25:19 +0530

    completed Tabs component

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-const Tabs = () => {
+const Tabs = ({todos}) => {
   const tabs = ['All','Open','Completed'];
   return (
     <nav className='tab-container'>
       {
         tabs.map((tab,index)=>{
+          const numOfTasks = tab === 'All'?
+          todos.length :
+          tab === 'Open'?
+          todos.filter(todo=> !todo.complete).length:
+          todos.filter(todo=> todo.complete).length;
           return(
             <button key={index} className='tab-button'>
-              <h4>{tab}<span>(0)</span></h4>
+              <h4>{tab}<span>({numOfTasks})</span></h4>
             </button>
           )
         })
